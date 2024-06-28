1aabd5bb60a5d10572152a81b383ee90d93e5382
jblee <jb.lee@genergyplm.com>
2024-06-28 15:58:35 +0900

    테스트 코드 작성을 위한 설정 변경

--- src/main/java/com/my/interrior/IDesignApplication.java
+++ src/main/java/com/my/interrior/IDesignApplication.java
@@ -2,8 +2,10 @@ package com.my.interrior;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IDesignApplication {
 
     public static void main(String[] args){
